c129aad343abfac2b3ac970805e723d9eec26cd0
Indrajit Sarkar <indrajit.sarkar@digitas.com>
2019-01-24 22:13:31 +0530

    homepage and issue fix

--- front-end/classic/demos/demo10/assets/demo/base/custom_script.js
+++ front-end/classic/demos/demo10/assets/demo/base/custom_script.js
@@ -1,3 +1,17 @@
 $(function () {
+	var _course = $('#course'),
+		_others = $('.others');
+
     $('#m_datepicker').datepicker();
+
+    _course.on('change', function() {
+    	var self = $(this).val();
+
+    	if (self === 'Others') {
+    		_others.removeClass('m--hide');
+    	} else {
+    		_others.addClass('m--hide');
+    	}
+    	console.log(self);
+    })
 });
